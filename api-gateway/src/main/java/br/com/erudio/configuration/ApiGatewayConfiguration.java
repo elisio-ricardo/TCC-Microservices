5f151b8efc6ecc026f38a31bf5ea3c9e245ed465
unknown <fabiofsilva92@gmail.com>
2022-06-20 21:54:52 -0300

    Ajuste livros para book-crud

--- api-gateway/src/main/java/br/com/erudio/configuration/ApiGatewayConfiguration.java
+++ api-gateway/src/main/java/br/com/erudio/configuration/ApiGatewayConfiguration.java
@@ -19,7 +19,7 @@ public class ApiGatewayConfiguration {
 //						.uri("http://httpbin.org:80"))
 				.route(p -> p.path("/cambio-service/**").uri("lb://cambio-service"))
 				.route(p -> p.path("/book-service/**").uri("lb://book-service"))
-				.route(p -> p.path("/livros/**").uri("lb://livros"))
+				.route(p -> p.path("/book-crud/**").uri("lb://book-crud"))
 				.build();
 	}
 
